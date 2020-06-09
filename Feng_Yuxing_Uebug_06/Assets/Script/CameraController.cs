bdf00ba76469536e47d4077342cbbfce186ffccd
YuxingF <yuxing.feng@stud.tu-darmstadt.de>
2020-06-09 18:53:06 +0800

    first commit

---
+++ Feng_Yuxing_Uebug_06/Assets/Script/CameraController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Camera cameraControl;
+    float fov;
+    public float minFov, maxFov;
+    public Transform playerTransform;
+    protected float speed;
+    protected float hSpeed = 0.50f;
+    protected float vSpeed = 1.0f;
+    protected Vector3 camOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.speed = 10f;
+        this.camOffset = cameraControl.transform.position - playerTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.ZoomCamer();
+        this.MoveCamer();
+    }
+    public void ZoomCamer()
+    {
+        fov += Input.GetAxis("Mouse ScrollWheel") * speed;
+        Mathf.Clamp(fov, minFov, maxFov);
+        cameraControl.fieldOfView = fov;
+    }
+    public void MoveCamer()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 currentCameraPos = cameraControl.transform.position;
+            float deltaX = Input.GetAxis("Mouse X") * hSpeed;
+            float deltaY = Input.GetAxis("Mouse Y") * vSpeed;
+            //move X
+            currentCameraPos.x += deltaX;
+            float angle = Vector3.Angle(cameraControl.transform.position - playerTransform.position, currentCameraPos - playerTransform.position);
+            this.cameraControl.transform.RotateAround(playerTransform.position, new Vector3(0f, 1f, 0f), angle);
+            currentCameraPos = cameraControl.transform.position;
+            //move 
+            if (currentCameraPos.y <= 16 && deltaY <= 0)
+                return;  
+            Debug.Log(cameraControl.fieldOfView);
+            currentCameraPos.y += deltaY;
+            angle = Vector3.Angle(cameraControl.transform.position - playerTransform.position, currentCameraPos - playerTransform.position);
+            Vector3 axisRotate = Vector3.Cross(cameraControl.transform.position - playerTransform.position, currentCameraPos - playerTransform.position).normalized;
+            currentCameraPos = cameraControl.transform.position;
+            this.cameraControl.transform.RotateAround(playerTransform.position, axisRotate, angle);
+            if (Input.GetKeyDown(KeyCode.F1))
+            {
+               
+                this.FollowPlayer();
+            }
+        }
+    }
+    public void FollowPlayer()
+    {
+        cameraControl.transform.position = playerTransform.position + this.camOffset;// Vector3.Slerp(transform.position, playerTransform.position + this.CamOffset, 0.5f);
+        transform.LookAt(playerTransform.position);
+    }
+}
---
+++ Feng_Yuxing_Uebug_06/Assets/Script/PlayerController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    public Camera cam;
+    [SerializeField] protected NavMeshAgent agent;
+    protected Animator animator;
+    protected Rigidbody rb;
+    protected Collider col;
+    bool isAttack;
+    protected bool onGround = false;
+    public UnityEvent landingEvent;
+   
+    void Start()
+    {
+        agent = GetComponentInChildren<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+       
+        col = GetComponent<CapsuleCollider>();     
+        isAttack = false;
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Attack();
+  
+   
+    }
+
+    public void Move()
+    {
+
+        if (!agent.enabled)
+            return;
+        if (  Input.GetMouseButtonDown(0))
+        {
+            RaycastHit target;
+            Ray targetRay = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(targetRay, out target))
+            {
+                if (isAttack == true)
+                {
+                    animator.SetBool("isAttack", false);
+                    isAttack = false;
+                    agent.isStopped = false;
+                }
+                agent.SetDestination(target.point);
+            }
+
+        }
+        if (agent.isOnNavMesh && agent.remainingDistance > agent.stoppingDistance)
+        {
+            animator.SetFloat("Speed", 1f);
+            this.NotifyCamWhenMoving();
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0f);
+        }
+
+
+    }
+
+    public void NotifyCamWhenMoving()
+    {
+        cam.GetComponent<CameraController>().FollowPlayer();
+    }
+    public void Rotate()
+    {
+        transform.forward = (agent.nextPosition - transform.position).normalized;
+        transform.LookAt(rb.position + transform.forward);
+    }
+
+    public void Attack()
+    {
+        if (Input.GetKey(KeyCode.Space) && isAttack == false)
+        {
+            if (agent.enabled)
+                agent.isStopped = true;
+            animator.SetBool("isAttack", true);
+            animator.SetFloat("Speed", 0f);
+            isAttack = true;
+        }
+    }
+
+
+            
+ }
+
+
